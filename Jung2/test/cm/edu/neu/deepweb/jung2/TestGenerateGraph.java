9fa7d57460f412849bf965d8687e4e4a9caa5e09
Eric wang <roymoro@gmail.com>
2013-04-01 16:39:15 +0800

    画图

--- Jung2/test/cm/edu/neu/deepweb/jung2/TestGenerateGraph.java
+++ Jung2/test/cm/edu/neu/deepweb/jung2/TestGenerateGraph.java
@@ -1,10 +1,22 @@
 package cm.edu.neu.deepweb.jung2;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
 import java.util.List;
 
+import javax.swing.JFrame;
+
+import org.apache.commons.collections15.Transformer;
+
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseMultigraph;
+import edu.uci.ics.jung.samples.SimpleGraphDraw;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 
 public class TestGenerateGraph {
 	public static void main(String[] args) {
@@ -19,5 +31,23 @@ public class TestGenerateGraph {
 		List<String> result=alg.getPath(1, 3);
 		System.out.println("path is:"+result.toString());
 		
+		Layout<Integer, String> layout=new CircleLayout<Integer, String>(g);
+		layout.setSize(new Dimension(300,300));
+		BasicVisualizationServer<Integer, String> vv=new BasicVisualizationServer<Integer, String>(layout);
+		vv.setPreferredSize(new Dimension(350,360));
+		vv.getRenderContext().setVertexFillPaintTransformer(new Transformer<Integer, Paint>() {
+	
+			@Override
+			public Paint transform(Integer arg0) {
+				// TODO Auto-generated method stub
+				return Color.GREEN;
+			}
+		});
+		vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller<String>());
+		JFrame frame=new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(vv);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
